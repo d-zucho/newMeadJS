4df3b3bc8f9a525f9169eeaedd62dd1b035fcade
D_Zucho <danny.nico.dev@outlook.com>
2021-05-03 21:52:18 -0700

    include async await

--- hangman/app.js
+++ hangman/app.js
@@ -6,22 +6,32 @@ puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 
 window.addEventListener('keypress', (e) => {
-    const guess = String.fromCharCode(e.charCode)
-    game1.makeGuess(guess)
-    puzzleEl.textContent = game1.puzzle
-    guessesEl.textContent = game1.statusMessage
+  const guess = String.fromCharCode(e.charCode)
+  game1.makeGuess(guess)
+  puzzleEl.textContent = game1.puzzle
+  guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle('2').then((puzzle) => {
+getPuzzle('2')
+  .then((puzzle) => {
     console.log(puzzle)
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(`Error: ${err}`)
-})
+  })
 
-getLocation().then((location) => {
+getLocation()
+  .then((location) => {
     return getCountry(location.country)
-}).then((country) => {
+  })
+  .then((country) => {
     console.log(country.name)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+/**
+ * - use getLocation to get country code
+ * - pass that code to getCountry to get full name
+ */
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,33 +1,33 @@
-const getPuzzle = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if (response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch puzzle')
-        }
-    }).then((data) => {
-        return data.puzzle
-    })
-}
+const getPuzzle = async (wordCount) => {
+  const response = await fetch(
+    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+  );
 
-const getCountry = (countryCode) => {
-    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
-        if (response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch data')
-        }
-    }).then((data) => {
-        return data.find((country) => country.alpha2Code === countryCode)
-    })
-}
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error("Unable to get puzzle");
+  }
+};
 
-const getLocation = () => {
-    return fetch('http://ipinfo.io/json?token=1a11bd55cc8f9c').then((response) => {
-        if (response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch location')
-        }
-    })
-}
+const getCountry = async (countryCode) => {
+  const response = await fetch("http://restcountries.eu/rest/v2/all");
+
+  if (response.status === 200) {
+    let data = await response.json();
+    return data.find((country) => country.apha2Code === countryCode);
+  } else {
+    throw new Error("unable to fetch");
+  }
+};
+
+const getLocation = async () => {
+  const response = await fetch("http://ipinfo.io/json?token=c5750751fcd945");
+
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error("ahhhh");
+  }
+};
